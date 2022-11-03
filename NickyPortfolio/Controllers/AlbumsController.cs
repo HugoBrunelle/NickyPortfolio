dbd9af07077b23cc9f54289ed8dd05dc1cfc29d5
Hugo <hugo_brunelle@hotmail.com>
2022-11-03 15:24:55 -0400

    Add project files.

---
+++ NickyPortfolio/Controllers/AlbumsController.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using NickyPortfolio.Data;
+using NickyPortfolio.Models;
+
+namespace NickyPortfolio.Controllers
+{
+    public class AlbumsController : Controller
+    {
+        private readonly NickyPortfolioDbContext _context;
+
+        public AlbumsController(NickyPortfolioDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Albums
+        public async Task<IActionResult> Index()
+        {
+              return View(await _context.Album.ToListAsync());
+        }
+
+        // GET: Albums/ShowSearchPage
+        public async Task<IActionResult> ShowSearchPage()
+        {
+            return View();
+        }
+
+        // GET: Albums/ShowSearchResult
+        public async Task<IActionResult> ShowSearchResult(String SearchPhrase)
+        {
+            return View("Index", await _context.Album.Where( a => a.Name.Contains(SearchPhrase)).ToListAsync());
+        }
+
+        // GET: Albums/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Album == null)
+            {
+                return NotFound();
+            }
+
+            var album = await _context.Album
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (album == null)
+            {
+                return NotFound();
+            }
+
+            return View(album);
+        }
+
+        // GET: Albums/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Albums/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,FilePath")] Album album)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(album);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(album);
+        }
+
+        // GET: Albums/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Album == null)
+            {
+                return NotFound();
+            }
+
+            var album = await _context.Album.FindAsync(id);
+            if (album == null)
+            {
+                return NotFound();
+            }
+            return View(album);
+        }
+
+        // POST: Albums/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,FilePath")] Album album)
+        {
+            if (id != album.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(album);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!AlbumExists(album.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(album);
+        }
+
+        // GET: Albums/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Album == null)
+            {
+                return NotFound();
+            }
+
+            var album = await _context.Album
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (album == null)
+            {
+                return NotFound();
+            }
+
+            return View(album);
+        }
+
+        // POST: Albums/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Album == null)
+            {
+                return Problem("Entity set 'NickyPortfolioDbContext.Album'  is null.");
+            }
+            var album = await _context.Album.FindAsync(id);
+            if (album != null)
+            {
+                _context.Album.Remove(album);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool AlbumExists(int id)
+        {
+          return _context.Album.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ NickyPortfolio/Models/Album.cs
@@ -0,0 +1,14 @@
+﻿namespace NickyPortfolio.Models
+{
+    public class Album
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string FilePath { get; set; }
+
+        public Album()
+        {
+
+        }
+    }
+}
