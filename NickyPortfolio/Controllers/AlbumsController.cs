b045bf866bc3940039ba94ae19fe6e28670d61b8
Hugo <hugo_brunelle@hotmail.com>
2022-11-09 14:12:52 -0500

    Load album images asynchronously

--- NickyPortfolio/Controllers/AlbumsController.cs
+++ NickyPortfolio/Controllers/AlbumsController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
@@ -16,9 +17,28 @@ namespace NickyPortfolio.Controllers
         }
 
         // GET: Albums
-        public IActionResult Index()
+        [HttpGet]
+        public IActionResult Index(string id)
         {
-              return View();
+            return View(model: id);
+        }
+
+        [HttpGet]
+        public JsonResult GetAlbum(string name)
+        {
+            // Verify if the album exist first
+            // Then perform the task
+            string albumPath = $"{Directory.GetCurrentDirectory()}\\Albums\\{name}";
+            string[] imagesPath = Directory.GetFiles(albumPath);
+
+            if (imagesPath.Length == 0)
+                return new JsonResult(Ok());
+
+            string[] imagesBase64 = imagesPath
+                .Select(i => Convert.ToBase64String(System.IO.File.ReadAllBytes(i)))
+                .ToArray();
+
+            return new JsonResult(Ok(imagesBase64));
         }
     }
 }
